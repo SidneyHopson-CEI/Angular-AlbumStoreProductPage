dfad1a9d6f842f77ff600158e99f4449693fca17
Sidney Hopson <shopson@ceiamerica.com>
2021-06-28 14:26:54 -0400

    Displaying a List of Products (has a bug)

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,13 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl : string = "../assets/album.json"
+  private _productUrl : string = "../assets/products.json"
 
   constructor(private _http : Http) { }
 
@@ -17,5 +19,11 @@ export class ProductService {
   {
     return this._http.get(this._albumUrl).map((response) =>
       <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>
+  {
+    return this._http.get(this._productUrl).map((response) =>
+    <Product[]>response.json());
   }
 }
